2b35a36a676d34bc9363af8aadc9c51c84721da3
Ariffansyah <harimato9@gmail.com>
2025-03-04 22:29:39 +0700

    feat: adding openlayers package and maps page

---
+++ SmartSafe/src/routes/(main)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+</script>
+
+<slot />
+<nav class="bg-blue-500 text-white text-center fixed bottom-0 w-full">
+    <div class="flex">
+        <button
+            class="hover:bg-blue-300 bg-blue-500 w-1/3 rounded text-white font-bold py-4"
+            on:click={() => goto("/maps")}
+        >
+            Maps
+        </button>
+        <button
+            class="hover:bg-blue-300 bg-blue-500 w-1/3 rounded text-white font-bold py-4"
+            on:click={() => goto("/notifications")}
+        >
+            Notifications
+        </button>
+        <button
+            class="hover:bg-blue-300 bg-blue-500 w-1/3 rounded text-white font-bold py-4"
+            on:click={() => goto("/profile")}
+        >
+            Profile
+        </button>
+    </div>
+</nav>
---
+++ SmartSafe/src/routes/(main)/maps/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { initMap } from "$lib/components/maps";
+    let mapElement: HTMLElement;
+
+    onMount(() => {
+        initMap(mapElement);
+    });
+</script>
+
+<main>
+    <section class="h-screen">
+        <div id="maps" class="h-full" bind:this={mapElement}></div>
+    </section>
+</main>
